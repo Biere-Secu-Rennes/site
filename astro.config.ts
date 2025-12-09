1e361a86397a1775ed0e1204eeeefc3160344714
Hedroed <contact@nathanryd.in>
2025-12-09 23:08:35 +0100

    new design

--- astro.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
-
-export default defineConfig({
-  site: 'https://localhost:3000/',
-  trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true }),],
-});
