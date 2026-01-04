236b7836bd9fa2543989c91591d643a3abd59569
Hedroed <contact@nathanryd.in>
2026-01-04 17:13:41 +0100

    add seo metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bieresecu.bzh',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
